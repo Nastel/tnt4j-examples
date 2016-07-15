b0f3907dee33eae39d3af6a00e655e2e839bf151
akausinis <andrius.kausinis@singleton-labs.lt>
2016-07-15 11:42:58 +0300

    path fix.

--- bin/filemon.bat
+++ bin/filemon.bat
@@ -3,7 +3,7 @@ setlocal
 set RUNDIR=%~p0
 
 set CLPATH=%RUNDIR%../tnt4j-samples.jar;%RUNDIR%../lib/*
-set MAINCL=com.nastel.jkool.tnt4j.samples.FolderMonitor
+set MAINCL=com.jkoolcloud.tnt4j.samples.FolderMonitor
 set TNT4JOPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=%RUNDIR%../config/tnt4j.properties
 java %TNT4JOPTS% -classpath %CLPATH% %MAINCL% %*
 endlocal
--- bin/filemon.sh
+++ bin/filemon.sh
@@ -2,6 +2,6 @@
 RUNDIR=`pwd`
 
 CLPATH="$RUNDIR/../tnt4j-samples.jar:$RUNDIR/../lib/*"
-MAINCL=com.nastel.jkool.tnt4j.samples.FolderMonitor
+MAINCL=com.jkoolcloud.tnt4j.samples.FolderMonitor
 TNT4JOPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dtnt4j.dump.on.vm.shutdown=true -Dtnt4j.dump.provider.default=true -Dtnt4j.config=$RUNDIR/../config/tnt4j.properties"
 java $TNT4JOPTS -classpath $CLPATH $MAINCL $@
